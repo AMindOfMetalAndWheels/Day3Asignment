153cb1d6f85eb8ce7f7e653aec3cb544ac804f44
Fabian Schleich <fabianschleich@gmx.net>
2024-11-20 21:29:19 +0100

    Updated

--- R/Developing.R
+++ R/Developing.R
@@ -7,34 +7,38 @@
 #
 
 
-install.packages("devtools")
-install.packages("usethis")
-install.packages("roxygen2")
-library(roxygen2)
-library(devtools)
-library(usethis)
+#install.packages("devtools")
+#install.packages("usethis")
+#install.packages("roxygen2")
+#library(roxygen2)
+#library(devtools)
+#library(usethis)
 
 #Description file first
-?usethis::use_author() # can be used to add author information
-usethis::use_author(given = "Test", family = "-Osteron", role = "CoAuthor")
+#?usethis::use_author() # can be used to add author information
+#usethis::use_author(given = "Test", family = "-Osteron", role = "cbt")
 
 #git installation done at intialization via point and click
 
-ls("package:usethis")
+#ls("package:usethis")
 
 #License done
-usethis::use_mit_license("advancedpackage")
+#usethis::use_mit_license("advancedpackage")
 
 #Packages used
-use_package("ggplot2")
-use_package("plotly")
+#use_package("ggplot2")
+#use_package("plotly")
 
 
 #Roxygen
-?use_roxygen_md
-use_roxygen_md(overwrite = TRUE)
-use_package("roxygen2")
+#?use_roxygen_md
+#use_roxygen_md(overwrite = TRUE)
+#use_package("roxygen2")
 
+
+?use_r()
+#use_r(name = "WHR")
+#use_r(name = "BMI")
 
 
 
