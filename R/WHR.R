dd5a4d862315912af4994aadf4d15bfc5f0e68b2
Fabian Schleich <fabianschleich@gmx.net>
2024-11-20 21:29:32 +0100

    updated

---
+++ R/WHR.R
@@ -0,0 +1,19 @@
+#########
+#Easy Waist to hip reatio function
+#waist devided by hip circumference measurement
+
+#' @param waist_lenght numeric value of waist circumference in cm
+#' @param hip_length numeric value of hip circumference in cm
+#' @return Waist hip ratio
+#' @examples
+#' WHR <- WHR_function(90, 95)
+#'
+
+WHR_function <- function (waist_lenght, hip_lenght) {
+  WHR <-waist_lenght / hip_lenght
+  return (WHR)
+}
+WHR <- WHR_function(90, 95)
+
+
+devtools::document()
